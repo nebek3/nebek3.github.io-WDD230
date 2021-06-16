b2f3de89fc3c95ffbc3f41ee75ab18fd35a972ec
nebek3 <neb14003@byui.edu>
2021-06-16 17:56:14 -0600

    fixnav

--- lesson6/windchill.js
+++ lesson6/windchill.js
@@ -1,15 +1,20 @@
 //f=35.74+0.6215t−35.75s0.16+0.4275ts0.16 
 
 let windc = document.getElementById("windchill");
-let t_string = document.getElementById("temp").innerText.match(/\d+/)[0];
-let s_string = document.getElementById("wind").innerText.match(/\d+/)[0];
+//let t_string = document.getElementById("temp").innerText.match(/\d+/)[0];
+//let s_string = document.getElementById("wind").innerText.match(/\d+/)[0];
+let t_string = document.getElementById("temp").innerHTML;
+let s_string = document.getElementById("wind").innerHTML;
+
+
+//USE a FUNCTION?
 
 let t = parsefloat(t_string);
 let s = parseFloat(s_string);
 
 if (s > 3 && t <= 50) {
-   // windc.innerText = Math.round(35.74+(0.6215*t)-(35.75*Math.pow(s, 0.16))+(0.4275*t*Math.pow(s, 0.16)));
-    document.getElementById('windchill').innerHTML = result;
+    windc.innerText = Math.round(35.74+(0.6215*t)-(35.75*Math.pow(s, 0.16))+(0.4275*t*Math.pow(s, 0.16)));
+    //document.getElementById('windchill').innerHTML = result;
 
 }
 else {
