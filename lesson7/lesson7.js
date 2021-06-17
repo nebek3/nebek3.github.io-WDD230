9985be03a31367485e7c2704fc4eda1d2e23ed08
nebek3 <neb14003@byui.edu>
2021-06-16 18:05:08 -0600

    navwork

--- lesson7/lesson7.js
+++ lesson7/lesson7.js
@@ -1,5 +1,5 @@
 function toggleMenu() {
-    document.getElementsByClassName("navigation")[0].classList.toggle("responsive")
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
 document.querySelector('#year').textContent = new Date().getFullYear();
 document.querySelector('#lastmod').textContent = document.lastModified;
