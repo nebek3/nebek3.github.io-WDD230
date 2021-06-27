0a6574af435d16a43e25be45bf90069df77f6c15
nebek3 <neb14003@byui.edu>
2021-06-26 18:45:50 -0600

    wk9fixes

---
+++ lesson10/js/lesson9.js
@@ -0,0 +1,58 @@
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+document.querySelector('#year').textContent = new Date().getFullYear();
+document.querySelector('#lastmod').textContent = document.lastModified;
+
+theDate = new Date();
+if (theDate.getDay() == 5) {
+    document.querySelector("#banner").style.display = "block";
+}
+
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
+
+requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        const towns = jsonObject["towns"];
+
+        const myTowns = towns.filter(x => x.name == "Preston" || x.name == "Soda Springs" || x.name == "Fish Haven");
+        console.log(myTowns);
+
+
+        myTowns.forEach(myTowns => {
+            let town = document.createElement("div");
+
+            let name = document.createElement("h2");
+            let motto = document.createElement("h4");
+            let founded = document.createElement("p");
+            let population = document.createElement("p");
+            let rainfall = document.createElement("p");
+            let pic = document. createElement("img");
+
+
+            name.textContent = myTowns.name;
+            motto.textContent = myTowns.motto;
+            founded.textContent = `Founded in ${myTowns.yearFounded}`;
+            population.textContent = `Current Population is ${myTowns.currentPopulation}`;
+            rainfall.textContent = `The Average Rainfall is ${myTowns.averageRainfall}`;
+            pic.setAttribute("src", `images/${town.photo}`);
+            pic.setAttribute("alt", `${myTowns.name}`)
+
+
+            town.append(name);
+            town.append(motto);
+            town.append(founded);
+            town.append(population);
+            town.append(rainfall);
+            town.append(pic);
+
+            document.querySelector("div.homemain").appendChild(town);
+        });
+    });
